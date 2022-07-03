e459376737ba14d72b79e54e1fef000f31af4bc5
Gonzalo Rumi <rumigonzalo@gmail.com>
2022-07-02 23:03:08 -0300

    add first back function

--- back/src/routes/player.js
+++ back/src/routes/player.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const { Player } = require('../db.js')
 
 // aca se construyen las rutas sobre router:
 
@@ -13,8 +14,28 @@ const dataPlayer = [
     }
 ]
 
-router.post("/", (req, res) => {
+router.post("/",  async (req, res) => {
+    let { id, officialName, group, members, password } = req.body
+
+    if (!id || !officialName || !group || !members || !password) res.json({error:true, message: "faltan datos"})
+
+    try {
     
+    const newPlayer = await Player.create({
+        id: id,
+        officialName: officialName,
+        group: group,
+        members: members,
+        password: password
+    })
+    
+    console.log(newPlayer)
+
+    res.send({error:false, message: "player created"})
+
+    } catch (e) {
+        res.send(e.message)
+    }
 })
 
 
