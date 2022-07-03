6ad34dd44f75de17e2d09e38489a39d5c7b54cb0
Gonzalo Rumi <rumigonzalo@gmail.com>
2022-07-02 21:40:17 -0300

    commit

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home () {
+
+    return (
+        <div>
+            Esto es una prueba de que funciona el ruteo
+            y ahora
+        </div>
+    )
+}
